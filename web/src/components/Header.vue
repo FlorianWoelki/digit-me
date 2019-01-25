fe47844f549b201f1ec1d6490fec4b847480880d
FlorianWoelki <FlorianWoelki@gmx.de>
2019-01-25 20:44:33 +0100

    Updated margin of style

--- web/src/components/Header.vue
+++ web/src/components/Header.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="text-xs-center">
+  <div class="header text-xs-center">
     <h1>Digit Me</h1>
     <h2>Machine Learning Digit Recognition</h2>
   </div>
 </template>
 
 <style>
+.header {
+  margin-bottom: 25px;
+}
 h1 {
   color: #ff4d4d;
   text-transform: uppercase;
